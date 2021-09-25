cb2294a176c94e76200ff4829c6e6619cf294192
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-09-25 23:05:07 +0200

    change

---
+++ vue-chat/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <router-link to="/home">Klik</router-link>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+// import Home from './views/Home.vue'
+export default {};
+</script>
+
+<style>
+</style>
---
+++ vue-chat/src/views/Home.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <div class="welcomeContainer">
+    <div class="card">
+      <div class="">
+        <h2 class="text">Welcome</h2>
+        <form @submit.prevent="enterChat()"><!--naziv funkcije mora da krene malim slovom enterChat-->
+          <label for="name">Enter your name:</label>
+          <input type="text" name="name" v-model="name" />
+          <button class="btn">Enter Chat</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+  data() {
+    return {
+      name: null,
+    };
+  },
+  methods:{
+      enterChat:function(){
+          console.log(this.name)
+      }
+  }
+};
+</script>
+
+<style>
+</style>
