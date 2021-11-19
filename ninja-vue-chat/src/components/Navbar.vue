5c2e98fef654e7b5f7b181468425a493e4d636ba
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-11-19 13:10:35 +0100

    change

--- ninja-vue-chat/src/components/Navbar.vue
+++ ninja-vue-chat/src/components/Navbar.vue
@@ -1,70 +1,68 @@
 <template>
-    <nav>
+  <nav>
+    <div class="navbar container" v-if="user">
+      
+        <div class="mainForm">
+            <div>
 
-  <div class="navbar container" v-if="user">
-      <div>
-      <p>Hey there {{user.displayName}}</p>
-      <p class="email">Curently logged in as {{user.email}}</p>
+          <p>Hey there {{ user.displayName }}</p>
+          <p class="email">Curently logged in as {{ user.email }}</p>
+            </div>
+        
+        <button class="" @click="logout">Logout</button>
       </div>
-      <button class="" @click="logout">Logout</button>
-    
-  <NewChatForm/>
-  </div>
-    </nav>
+      <div class="newChat">
+        <NewChatForm />
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
-import {projectAuth} from './../firebase/config'
-import NewChatForm from './NewChatForm.vue'
+import { projectAuth } from "./../firebase/config";
+import NewChatForm from "./NewChatForm.vue";
 // import {useRouter} from 'vue-router'
 
-import {ref} from 'vue';
-const user = ref(projectAuth.currentUser)
+import { ref } from "vue";
+const user = ref(projectAuth.currentUser);
 
-
-
-    projectAuth.onAuthStateChanged(user1=>{//ovim na svaku promenu dohvatamo trenutnog korisnika, stavljamo ga u promenljivu 'user', preko
-    //ovog ref gore, vrednost promenljive user stavljamo u projectAuth.currentUser. Na kraju, dole u data(), u promenljivu user, koja je
-    //klasicna vue promenljiva, ne kao ova gore definisana, stavljamo projectAuth.currentUser u kojoj se nalaze podaci koje zelimo
-        user.value = user1
-        console.log(user1)
-       
-        
-        });
-
+projectAuth.onAuthStateChanged((user1) => {
+  //ovim na svaku promenu dohvatamo trenutnog korisnika, stavljamo ga u promenljivu 'user', preko
+  //ovog ref gore, vrednost promenljive user stavljamo u projectAuth.currentUser. Na kraju, dole u data(), u promenljivu user, koja je
+  //klasicna vue promenljiva, ne kao ova gore definisana, stavljamo projectAuth.currentUser u kojoj se nalaze podaci koje zelimo
+  user.value = user1;
+  console.log(user1);
+});
 
 export default {
-    components:{
-        NewChatForm:NewChatForm
-    },
-    data(){
-        return{
-            user:projectAuth.currentUser
-        }
-    },
-methods:{ 
-
-    logout(){
-        projectAuth.signOut()
-        .then(()=>{
-            console.log('user is logedout')
-            this.user=projectAuth.currentUser
-            this.$router.push({name:'Welcome'})
+  components: {
+    NewChatForm: NewChatForm,
+  },
+  data() {
+    return {
+      user: projectAuth.currentUser,
+    };
+  },
+  methods: {
+    logout() {
+      projectAuth
+        .signOut()
+        .then(() => {
+          console.log("user is logedout");
+          this.user = projectAuth.currentUser;
+          this.$router.push({ name: "Welcome" });
         })
-        .catch((err)=>{
-            console.log(err)
-        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
-    getData(){
-        console.log(this.user)
-    }
-}
-
-
-}
-
+    getData() {
+      console.log(this.user);
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
 
 </style>
--- ninja-vue-chat/src/components/NewChatForm.vue
+++ ninja-vue-chat/src/components/NewChatForm.vue
@@ -1,31 +1,56 @@
 <template>
   <form action="">
-
-      <textarea @keypress.enter.prevent="handleSubmit"  name="" id="" cols="30" rows="10" placeholder="type message here..." v-model="message"></textarea>
+    <textarea
+      @keypress.enter.prevent="handleSubmit"
+      name=""
+      id=""
+      cols="30"
+      rows="1"
+      placeholder="Type message here..."
+      v-model="message"
+    ></textarea>
   </form>
 </template>
 
 <script>
-import {projectAuth, timestamp} from './../firebase/config.js'
+import { projectAuth, timestamp } from "./../firebase/config.js";
 export default {
-data(){
-  return{
-    message:'neki'
-        }
-},
-methods:{
-  handleSubmit(){
-    const chat = {
-      name:projectAuth.currentUser.displayName,
-      message:this.message,
-      createdAt:timestamp()
-    }
-    console.log(chat);
-  }
-}
-}
+  data() {
+    return {
+      message: "",
+    };
+  },
+  methods: {
+    handleSubmit() {
+      const chat = {
+        name: projectAuth.currentUser.displayName,
+        message: this.message,
+        createdAt: timestamp(),
+      };
+      console.log(chat);
+    },
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+form {
+  margin: 1rem;
+  textarea {
+    width: 100%;
+    max-width: 100%;
+    margin-bottom: 0.6rem;
+    padding: 1rem;
+    border: none;
+    border-bottom: 0.1rem solid rgba(black,.1);
+    font-family: inherit;
+    outline: none;
+    font-size: 1.5rem;
 
+    &:focus{
+      border-bottom: 0.1rem solid black;
+    }
+  }
+  
+}
 </style>
