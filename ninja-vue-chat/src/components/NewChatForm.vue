85b8f01c919572735eefba610e9361c7b1ba1036
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-11-22 19:59:58 +0100

    change

--- ninja-vue-chat/src/components/NewChatForm.vue
+++ ninja-vue-chat/src/components/NewChatForm.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="chat-window" v-if="documents">
+  <div ref="chat" class="chat-window" v-if="documents">
     <div class="single" v-for="doc in documents" :key="doc.id">
-    <span class="created-at">{{doc.data.createdAt.toDate()}}</span>
-    <span class="name">{{doc.data.name}}</span>
-    <span class="message">{{doc.data.message}}</span>
+      <span class="created-at">{{ doc.createdAt.toDate() }}</span>
+      <span class="name">{{ doc.name }}</span>
+      <span class="message">{{ doc.message }}</span>
     </div>
   </div>
   <form action="">
@@ -17,75 +17,77 @@
       v-model="message"
     ></textarea>
   </form>
-  <div class="err">{{err}}</div>
+  <div class="err">{{ err }}</div>
 </template>
 
 <script>
-import { projectFirestore,projectAuth, timestamp } from "./../firebase/config.js";
-import {ref} from 'vue'
-let document = [];
-const documents = ref(document)
-    projectFirestore.collection('messages').orderBy('createdAt')
-      .onSnapshot(snap=>{
-        snap.docs.forEach(doc=>{
-         doc.data().createdAt && documents.value.push({data:doc.data(), id:doc.id})
-        })
-      })
-
-
+import {
+  projectFirestore,
+  projectAuth,
+  timestamp,
+} from "./../firebase/config.js";
 export default {
   data() {
     return {
       message: "",
-      err:'',
-      documents:document
+      err: "",
+      documents: [],
     };
   },
   methods: {
     handleSubmit() {
+      console.log(this.documents, 'sss')
       const chat = {
         name: projectAuth.currentUser.displayName,
         message: this.message,
-        createdAt: timestamp()
+        createdAt: timestamp(),
       };
-      projectFirestore.collection('messages').add(chat)
-      .then(data=>{
-         this.message='';
-         console.log(data)
-      }).catch(err=>{
-        this.err=err
-      })
-     
-    }
-  },
+      this.documents=[]
+      projectFirestore
+        .collection("messages")
+        .add(chat)
+        .then((data) => {
+          this.message = "";
+          console.log(data);
+        })
+        .catch((err) => {
+          this.err = err;
+        });
+    },
+  }
+  ,
+  mounted(){
+   projectFirestore.collection('messages').orderBy('createdAt')
+     .onSnapshot(snap=>{
+       this.documents=[]
+       snap.docs.forEach(doc=>{
+        doc.data().createdAt && this.documents.push({...doc.data(), id:doc.id})
+       })
+      //  documents.value = document
+     })
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-
-
-  .chat-window{
-    background: #fafafa;
-    padding: 3rem 2rem;
-    overflow: hidden;
-    .single{
-      margin: 1.8rem 0;
-
-      .created-at{
-        display: block;
-        color: #999;
-        font-size: 1.2rem;
-        margin-bottom: .4rem;
-      }
-      .name{
-        font-weight: bold;
-        margin-right: .6rem;
-      }
-     
+.chat-window {
+  background: #fafafa;
+  padding: 3rem 2rem;
+  overflow: hidden;
+  .single {
+    margin: 1.8rem 0;
+    .created-at {
+      display: block;
+      color: #999;
+      font-size: 1.2rem;
+      margin-bottom: 0.4rem;
+    }
+    .name {
+      font-weight: bold;
+      margin-right: 0.6rem;
     }
   }
-
-
+}
 form {
   margin: 1rem;
   textarea {
@@ -94,18 +96,13 @@ form {
     margin-bottom: 0.6rem;
     padding: 1rem;
     border: none;
-    border-bottom: 0.1rem solid rgba(black,.1);
+    border-bottom: 0.1rem solid rgba(black, 0.1);
     font-family: inherit;
     outline: none;
     font-size: 1.5rem;
-
-    &:focus{
+    &:focus {
       border-bottom: 0.1rem solid black;
     }
   }
-
-  
 }
-
-
 </style>
