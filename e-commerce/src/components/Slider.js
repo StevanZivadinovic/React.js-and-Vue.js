5fb0a0ec27887ae60acb87b27e54e91c0fc6df68
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-12-10 23:04:01 +0100

    change

--- e-commerce/src/components/Slider.js
+++ e-commerce/src/components/Slider.js
@@ -2,19 +2,26 @@ import React from "react";
 import './../style/slider.scss'
 import fashion1 from './../img/fashion1.png'
 const Slider = () => {
+
+  const handleLeft = () =>{
+    
+    document.querySelector('.wrapper').style.transform = `translateX(${-100}vw)`;
+    console.log('haj')
+  }
+
   return (
     <div className="mainSlider">
       <div className="arrows">
-        <span className="arrow left">
+        <span className="arrow left" onClick={()=>{handleLeft()}}>
           <i className="fas fa-arrow-left"></i>
         </span>
-       {/* <div className="wrapper">
+       <div className="wrapper">
           <div className="slide">
             <div className="imageContainer">
               <img src={fashion1}/>
             </div>
             <div className="infoContainer">
-             <div className="title">Naslov</div>
+             <div className="title">summer sale</div>
              <div className="textInfo">Some random text...</div>
              <button className="button">Show now</button>
 
@@ -27,7 +34,7 @@ const Slider = () => {
               <img src={fashion1}/>
             </div>
             <div className="infoContainer">
-             <div className="title">Naslov</div>
+             <div className="title">winter sale</div>
              <div className="textInfo">Some random text...</div>
              <button className="button">Show now</button>
 
@@ -40,14 +47,14 @@ const Slider = () => {
               <img src={fashion1}/>
             </div>
             <div className="infoContainer">
-             <div className="title">Naslov</div>
+             <div className="title">spring summer</div>
              <div className="textInfo">Some random text...</div>
              <button className="button">Show now</button>
 
             </div>
           </div>
 
-          </div> */}
+          </div>
         <span className="arrow right">
           <i className="fas fa-arrow-right"></i>
         </span>
