8a09acf45b8ae2a17c1510cdf36dc7ab6949165f
StevanZivadinovic <stevanzivadinovic@outlook.com>
2022-04-17 20:51:42 +0200

    change

--- contact-manager/src/components/prvaComponenta.js
+++ contact-manager/src/components/prvaComponenta.js
@@ -5,42 +5,35 @@ export default function PrvaComponenta() {
 
     let navigator = useNavigate();
     const [recnik, setRecnik] = useState([]);
-    const username = 'steva94';
-    const password = 'andjelanole1994'
-    const  url = new URL("https://dictapi.lexicala.com/languages") ;
+    // const username = 'steva94';
+    // const password = 'andjelanole1994'
+    // const  url = new URL("https://dictapi.lexicala.com/languages") ;
 
-    url.search = new URLSearchParams({'username':username, 'password':password});
-    // url.search = new URLSearchParams(password);
-    // let params = new URLSearchParams(url.search);
+    // url.search = new URLSearchParams({'username':username, 'password':password});
+    // // url.search = new URLSearchParams(password);
+    // // let params = new URLSearchParams(url.search);
   
-    // params.append("username", username)
-    // params.append("password", password);
-    console.log(url)
+    // // params.append("username", username)
+    // // params.append("password", password);
+    // console.log(url)
 
 
-    useEffect(() => {
-
-
-/////s///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        // fetch(url
-
-        //    )
-        //     .then((response) => {
-        //       if (response.ok) {
-        //         response.json().then((data) => {
-        //        return data.json()
-        //         });
-                
-        //       }
-        //     })
-        //     .then((data)=>{
-        //         console.log(data)
-        //     })
-        //     .catch((error) => {
-        //       console.log(error);
-        //     });
-       
-      }, [])
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Host': 'lexicala1.p.rapidapi.com',
+        'X-RapidAPI-Key': 'd3d8416290mshe8f889303ad46e5p16190ajsn798c21993e0d'
+      }
+    };
+    useEffect(()=>{
+      fetch('https://lexicala1.p.rapidapi.com/search?source=global&language=ru', options)
+        .then(response => response.json())
+        .then(response => {
+          console.log(response)
+          setRecnik(response)
+        })
+        .catch(err => console.error(err));
+    },[])
     
   return (
     <div>
