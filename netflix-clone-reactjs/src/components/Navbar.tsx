83cbad2b41d40695ed2a75cfd3ff9bde07d9f691
StevanZivadinovic <stevanzivadinovic@outlook.com>
2022-06-12 23:01:36 +0200

    add navbar

---
+++ netflix-clone-reactjs/src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <h1 className='text-white'>NETFLIX</h1>
+    <div>
+        <button>SIGN IN</button>
+        <button>SIGN UP</button>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
