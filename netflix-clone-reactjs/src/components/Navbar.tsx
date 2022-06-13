927d229d80429247af084beb245053bbc76196ba
StevanZivadinovic <stevanzivadinovic@outlook.com>
2022-06-13 16:06:43 +0200

    add router

--- netflix-clone-reactjs/src/components/Navbar.tsx
+++ netflix-clone-reactjs/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between p-4 absolute w-full z-[100]'>
+    <div className='flex items-center justify-between p-4  w-full z-[100]'>
     <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
     <div>
         <button className='text-white pr-4 text-xl cursor-pointer'>Sign in</button>
