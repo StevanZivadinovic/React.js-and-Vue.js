b9685c553f738b31d27611be6ad3352842bdee42
StevanZivadinovic <stevanzivadinovic@outlook.com>
2022-06-17 18:06:13 +0200

    add firebase and useConfig

--- netflix-clone-reactjs/src/App.tsx
+++ netflix-clone-reactjs/src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 // @ts-ignore
+import { AuthContextProvider } from "./components/AuthContext/AuthContext.tsx";
+// @ts-ignore
 import Layout from "./components/Layout/Layout.tsx";
 // @ts-ignore
 import routes from "./config/routes.ts";
@@ -9,9 +11,11 @@ import routes from "./config/routes.ts";
 
 
 function App() {
+
+  console.log(AuthContextProvider.value);
   return (
     <>
-    
+    <AuthContextProvider>
     <Layout routes={routes}>
      <React.Suspense>
         <Routes>
@@ -20,7 +24,7 @@ function App() {
      
       </React.Suspense>
       </Layout>
-
+      </AuthContextProvider>
     </>
   );
 }
---
+++ netflix-clone-reactjs/src/components/AuthContext/AuthContext.tsx
@@ -0,0 +1,48 @@
+import {createContext, useContext, useEffect, useState} from 'react';
+import {auth} from '../../config/Firebase';
+
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged
+} from 'firebase/auth';
+
+interface IContext {
+    // signUp:object,
+    // user:object
+}
+
+export const AuthContext = createContext<IContext>({});
+
+export function AuthContextProvider({children}){
+
+    const [user, setUser] = useState({});
+
+    function signUp(email:string,password:string){
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    function logIn(email:string, password:string){
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    function logOut(){
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser)=>{
+            setUser(currentUser!);
+        })
+        return(()=>{
+            unsubscribe();
+        })
+    }, [])
+    
+
+   return <AuthContext.Provider value={{signUp, logIn, logOut, user}}>{children}</AuthContext.Provider>
+}
+
+export function User() {
+      
+    return useContext(AuthContext);
+  }
