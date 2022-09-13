4af906fc95bfb1df39190b0c3ebedad604b7c479
StevanZivadinovic <stevanzivadinovic@outlook.com>
2022-09-13 20:24:55 +0200

    update delete

--- e-commerce/src/pages/Bag.js
+++ e-commerce/src/pages/Bag.js
@@ -1,116 +1,116 @@
-import { collection, onSnapshot, query } from 'firebase/firestore';
+import { collection, onSnapshot, query } from "firebase/firestore";
 import { doc, deleteDoc } from "firebase/firestore";
-import React, { useEffect, useRef, useState } from 'react'
-import { db } from '../config/firebase';
-import listsOfImage from '../helperFunc/images';
-import './../style/bug.scss'
+import React, { useEffect, useRef, useState } from "react";
+import { db } from "../config/firebase";
+import listsOfImage from "../helperFunc/images";
+import "./../style/bug.scss";
 
 export const Bag = () => {
-    const [productToBuy, setProductToBuy] = useState([]);
-    const [noDataText, setnoDataText] = useState('The bag is empty!')
-    const productListDiv = useRef()
+  const [productToBuy, setProductToBuy] = useState([]);
+  const [noDataText, setnoDataText] = useState("The bag is empty!");
+  const productListDiv = useRef();
 
-
-    let q = query(collection(db, `bag`));
+  let q = query(collection(db, `bag`));
 
   useEffect(() => {
     const controller = new AbortController();
-    
-    setProductToBuy([]);
+
+   
     onSnapshot(q, (snapshot) => {
-      console.log('okinuto')
+      setProductToBuy([]);
       snapshot.docs.map((doc) => {
         let c = doc.data();
-        c['docID']=doc.id;
+        c["docID"] = doc.id;
         setProductToBuy((productToBuy) => [...productToBuy, c]);
       });
     });
 
-    return (()=>{
+    return () => {
       controller.abort();
-    })
+    };
   }, []);
 
-  const handleDeleteProductFromBag = (docBag) =>{
-console.log(docBag)
+  const handleDeleteProductFromBag = (docBag) => {
+    console.log(docBag);
     onSnapshot(q, (snapshot) => {
       snapshot.docChanges().forEach((docFromBagDB) => {
-        if(docFromBagDB.doc.id=== docBag.docID){
-          console.log(docFromBagDB.doc.id, docBag.docID)
+        if (docFromBagDB.doc.id === docBag.docID) {
+          console.log(docFromBagDB.doc.id, docBag.docID);
           deleteDoc(doc(db, "bag", docFromBagDB.doc.id))
-          .then(()=>{
-          // setProductToBuy([])
-        
-          
-          console.log('data is deleted')
-        }).catch((err)=>{
-          console.log(err);
-        })
-        
-      }else{
-       
-        console.log('There is no sach product')
-      }
+            .then(() => {
+              console.log("data is deleted");
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        } else {
+          console.log("There is no sach product");
+        }
       });
-    })
-
-}
+    });
+  };
 
   return (
-    <div className='mainBug'>
-        <h1 className='mainBugTitle'>Bag</h1>
-        <h2 className='emptyBagTitle'>{productToBuy.length==0 ? noDataText : ''}</h2>
-        <div className="mainBugContent">
-        <div className=''>
-        {productToBuy.map((a, i) => {
-              return (
-                <div className="product" key={i} ref={productListDiv}>
-                  <div className="productDetail">
-                    <img src={listsOfImage[i]} alt="" />
-                    <div className="details">
-                      <div className="productName">
-                        <b>Product:</b> {a.productName}
-                      </div>
-                      <span className="productId">
-                        <b>ID:</b> {a.id}
-                      </span>
-                      <div style={{fontSize:'2rem'}} className='colorDiv'
-                        
-                      >Color: <span className="productColor"
-                      style={{ backgroundColor: `${a.color}` }}></span></div>
-                      <span className="productSize">
-                        {" "}
-                        <b>Size: </b>
-                        {a.size}
-                      </span>
+    <div className="mainBug">
+      <h1 className="mainBugTitle">Bag</h1>
+      <h2 className="emptyBagTitle">
+        {productToBuy.length == 0 ? noDataText : ""}
+      </h2>
+      <div className="mainBugContent">
+        <div className="">
+          {productToBuy.map((a, i) => {
+            return (
+              <div
+                data-id={a.docID}
+                className="product"
+                key={i}
+                ref={productListDiv}
+              >
+                <div className="productDetail">
+                  <img src={listsOfImage[i]} alt="" />
+                  <div className="details">
+                    <div className="productName">
+                      <b>Product:</b> {a.productName}
                     </div>
-                  </div>
-                  <div className="priceDetail">${a.price}</div>
-                </div>
-              );
-            })}
-        </div>
-        <div className="summaryContainer">
-            {productToBuy.map((a,i) => {
-              {
-                return (
-                  <div className="summary" key={i}>
-                    {a.summary}
-                    <span>
-                      <button 
-                      // data-id={a.doc.id}
-                      onClick={(e) => handleDeleteProductFromBag(a)}
-                      >
-                        Delete
-                      </button>
+                    <span className="productId">
+                      <b>ID:</b> {a.id}
+                    </span>
+                    <div style={{ fontSize: "2rem" }} className="colorDiv">
+                      Color:{" "}
+                      <span
+                        className="productColor"
+                        style={{ backgroundColor: `${a.color}` }}
+                      ></span>
+                    </div>
+                    <span className="productSize">
+                      {" "}
+                      <b>Size: </b>
+                      {a.size}
                     </span>
                   </div>
-                );
-              }
-            })}
-          </div>
+                </div>
+                <div className="priceDetail">${a.price}</div>
+              </div>
+            );
+          })}
         </div>
-       
+        <div className="summaryContainer">
+          {productToBuy.map((a, i) => {
+            {
+              return (
+                <div data-id={a.docID} className="summary" key={i}>
+                  {a.summary}
+                  <span>
+                    <button onClick={(e) => handleDeleteProductFromBag(a)}>
+                      Delete
+                    </button>
+                  </span>
+                </div>
+              );
+            }
+          })}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
