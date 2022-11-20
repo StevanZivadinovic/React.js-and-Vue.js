4c7b006b7c5a077b0c237b76960804f0ece8ea46
StevanZivadinovic <stevanzivadinovic@outlook.com>
2022-11-20 16:33:17 +0100

    set gallery

--- Next.js website/next.js_website/components/Slider.js
+++ Next.js website/next.js_website/components/Slider.js
@@ -1,38 +1,56 @@
-import Image from 'next/image'
-import React, { useState } from 'react'
-import { SliderData } from './Slider.data'
-import {FaArrowCircleLeft, FaArrowCircleRight} from 'react-icons/fa'
+import Image from "next/image";
+import React, { useState } from "react";
+import { SliderData } from "./Slider.data";
+import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
 const Slider = () => {
-    const [current, setCurrent]=useState(0);
+  const [current, setCurrent] = useState(0);
 
-    const nextSlide = ()=>{
-        console.log('next');
-    }
+  const nextSlide = () => {
+    setCurrent(SliderData.length - 1 === current ? 0 : current + 1);
+  };
 
-    const previousSlide = ()=>{
-        console.log('previous');
-    }
+  const previousSlide = () => {
+    setCurrent(current === 0 ? SliderData.length - 1 : current - 1);
+  };
   return (
-
-    <div>
-        <h1>Gallery</h1>
-        <div className="gallery">
-            <div className="">
-            <FaArrowCircleLeft 
-            onClick={previousSlide}
-            size={50}
-            />
-            {SliderData.map((img, i)=>{
-              return <Image src={img.image} width='1440' height='600' key={i} style={{objectFit:'cover', marginTop:'1rem', opacity:`${current===i ? 1 :0}`}} alt='image'/>
-            })}
-            <FaArrowCircleRight 
-            onClick={nextSlide}
-            size={50}
-            />
+    <div id="gallery" className="max-w-[1240px] mx-auto">
+      <h1 className="text-2xl font-bold text-center p-4">Gallery</h1>
+      <div className="relative flex justify-center p-4">
+        {SliderData.map((img, i) => {
+          return (
+            <div
+              className={
+                i === current
+                  ? "opacity-[1] ease-in duration-1000"
+                  : "opacity-0"
+              }
+              key={i}
+            >
+              <FaArrowCircleLeft
+                onClick={previousSlide}
+                size={50}
+                className="absolute top-[50%] left-[30px] cursor-pointer text-white/70 select-none z-[2] "
+              />
+              {i === current && (
+                <Image
+                  src={img.image}
+                  width="1440"
+                  height="600"
+                  alt="image"
+                  style={{ objectFit: "cover" }}
+                />
+              )}
+              <FaArrowCircleRight
+                onClick={nextSlide}
+                size={50}
+                className="absolute top-[50%] right-[30px] cursor-pointer text-white/70 select-none z-[2]"
+              />
             </div>
-        </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
