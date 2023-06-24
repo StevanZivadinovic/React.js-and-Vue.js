6c47dc103c387a2db37a3f4706528785d1b054c4
StevanZivadinovic <stevanzivadinovic@outlook.com>
2023-06-24 16:43:56 +0200

    update kontakt.js file

--- pravoslavne-ikone/src/components/kontakt.js
+++ pravoslavne-ikone/src/components/kontakt.js
@@ -1,19 +1,16 @@
 import React, { useState } from "react";
 import emailjs from "emailjs-com";
-// import{ init } from '@emailjs/browser';
-
-import Footer from './footer'
-import fb from "./../assets/facebook.svg";
+import Footer from "./footer";
 import "./../style/kontakt.scss";
 import { connect } from "react-redux";
-import {wholeStateToFalse} from './../actions/wholeStateToFalse'
-import {ValidationInputTextareaTrue, ValidationInputTextareaFalse} from './../actions/validationInputTextarea'
+import { wholeStateToFalse } from "./../actions/wholeStateToFalse";
+import {
+  ValidationInputTextareaTrue,
+  ValidationInputTextareaFalse,
+} from "./../actions/validationInputTextarea";
 
-
-// import { useForm } from "react-hook-form";
-
- function Kontakt({statusIzReduxa, setWholeStatusToFalse, actionValidationInputTextareaTrue1, ValidationInputTextareaFalse1}) {
-  //  console.log(statusIzReduxa, setWholeStatusToFalse)
+function Kontakt({
+}) {
   const [status, setStatus] = useState({
     textarea: false,
     phone: false,
@@ -21,10 +18,9 @@ import {ValidationInputTextareaTrue, ValidationInputTextareaFalse} from './../ac
     validationEmail: false,
     validationInputSubject: false,
   });
-  // init("user_iDF7GBVBepZlv2bZg187d");
+
   let labelPosition = (position, display) => {
     let label = document.querySelector("label");
-    // label.style.opacity = opacit;
     label.style.position = "absolute";
     label.style.top = `${position}px`;
     label.style.right = "0px";
@@ -35,26 +31,15 @@ import {ValidationInputTextareaTrue, ValidationInputTextareaFalse} from './../ac
     label.style.opacity = 1;
   };
 
-  
-
   function onClickHandle() {
-    console.log(status.validationInputSubject &&
-      status.textarea &&
-      status.phone &&
-      status.validationInput &&
-      status.validationEmail);
-
     alert("Порука је послата!");
   }
 
   function onClickHandle1() {
-    console.log(status);
-
     alert("Емаил је неисправан!");
   }
 
   function sendEmail(e) {
-    console.log('hay hay')
     e.preventDefault();
 
     emailjs
@@ -66,7 +51,6 @@ import {ValidationInputTextareaTrue, ValidationInputTextareaFalse} from './../ac
       )
       .then(
         (result) => {
-          console.log(result);
           setStatus({
             ...status,
             textarea: false,
@@ -76,61 +60,46 @@ import {ValidationInputTextareaTrue, ValidationInputTextareaFalse} from './../ac
             validationInputSubject: false,
           });
           // setWholeStatusToFalse()
-          console.log(result);
         },
         (error) => {
           console.log(error.text);
         }
       ); //ova funkcija je kopirana sa njihovog sajta
-    console.log(e.target);
+
     e.target.reset();
   }
-  // validationInputTextarea()
-  // validationInputPhone()
-  // validationInputName()
-  // validationInputSubject()
-  // validationEmail()
-
-
   //npm install emailjs-com --save, ovo mora da se instalira da bi EmailJS dodatak radio
 
-  
   let validationInputTextarea = (e) => {
     var pattern =
       /^[\s0-9-,.AБВГДЂЕЖЗИЈКЛЉМНЊОПРСТЋУФХЦЧЏШабвгдђежзијклљмнњопрстћуфхцчџшA-Za-zA-Ša-š:/.!?]+$/;
     let a = pattern.test(e.target.value);
-    console.log(a);
 
-    if (a)  {
+    if (a) {
       e.target.style.borderColor = "blue";
-     
+
       // actionValidationInputTextareaTrue1()
       setStatus({ ...status, textarea: true });
-      console.log(status);
+
       e.target.setCustomValidity("");
-
-  
-
-    } 
-    else  {
+    } else {
       e.target.setCustomValidity("Порука је празна");
 
       e.target.style.borderColor = "red";
       // ValidationInputTextareaFalse1()
-   
+
       setStatus({ ...status, textarea: false });
     }
   };
   let validationInputPhone = (e) => {
     var pattern = /^[\s0-9-]+$/;
     let a = pattern.test(e.target.value);
-    console.log(a);
 
     if (a) {
       setStatus({ ...status, phone: true });
       e.target.style.borderColor = "blue";
       e.target.setCustomValidity("");
-    } else  {
+    } else {
       e.target.setCustomValidity("Молимо Вас не користите словне карактере.");
 
       setStatus({ ...status, phone: true });
@@ -143,12 +112,11 @@ import {ValidationInputTextareaTrue, ValidationInputTextareaFalse} from './../ac
     var pattern =
       /^[\s-,.AБВГДЂЕЖЗИЈКЛЉМНЊОПРСТЋУФХЦЧЏШабвгдђежзијклљмнњопрстћуфхцчџшA-Za-zA-Ža-ž:/.!?]+$/;
     let a = pattern.test(e.target.value);
-    console.log(a);
 
-    if (a)  {
+    if (a) {
       setStatus({ ...status, validationInput: true });
       e.target.style.borderColor = "blue";
-      labelPosition("0", 'none');
+      labelPosition("0", "none");
       e.target.setCustomValidity("");
     } else {
       e.target.setCustomValidity(
@@ -157,7 +125,7 @@ import {ValidationInputTextareaTrue, ValidationInputTextareaFalse} from './../ac
       setStatus({ ...status, validationInput: false });
       e.target.style.borderColor = "red";
 
-      labelPosition("50", 'inline');
+      labelPosition("50", "inline");
     }
   };
 
@@ -165,50 +133,43 @@ import {ValidationInputTextareaTrue, ValidationInputTextareaFalse} from './../ac
     var pattern =
       /^[\s0-9-,.AБВГДЂЕЖЗИЈКЛЉМНЊОПРСТЋУФХЦЧЏШабвгдђежзијклљмнњопрстћуфхцчџшA-Za-zA-Ša-š:/.!?]+$/;
     let a = pattern.test(e.target.value);
-    console.log(a);
 
     if (a) {
       setStatus({ ...status, validationInputSubject: true });
       e.target.style.borderColor = "blue";
-      labelPosition("0", 'none');
+      labelPosition("0", "none");
       e.target.setCustomValidity("");
-    } else{
+    } else {
       setStatus({ ...status, validationInputSubject: false });
       e.target.style.borderColor = "red";
-      labelPosition("230", 'inline');
+      labelPosition("230", "inline");
     }
   };
 
   let validationEmail1 = (e) => {
     var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
     let a = pattern.test(e.target.value);
-    console.log(a);
 
     if (a) {
       e.target.style.borderColor = "blue";
       setStatus({ ...status, validationEmail: true });
-      document.querySelector(
-        'input[type="submit"]'
-      ).disabled = false
-      
-      labelPosition("110", 'none');
+      document.querySelector('input[type="submit"]').disabled = false;
+
+      labelPosition("110", "none");
       e.target.setCustomValidity("");
     } else {
       e.target.setCustomValidity("Е-маил је неисправан");
 
       setStatus({ ...status, validationEmail: false });
       e.target.style.borderColor = "red";
-      document.querySelector(
-        'input[type="submit"]'
-      ).disabled = true
+      document.querySelector('input[type="submit"]').disabled = true;
       let dugme1 = document.querySelector('input[type="submit"]');
-      labelPosition("110", 'inline');
+      labelPosition("110", "inline");
     }
-    console.log(status);
   };
 
   return (
-    <div className="main3" >
+    <div className="main3">
       {/* onSubmit={handleSubmit(onSubmit)} */}
       <form
         className="wrapper"
@@ -219,10 +180,9 @@ import {ValidationInputTextareaTrue, ValidationInputTextareaFalse} from './../ac
           status.validationInput &&
           status.validationEmail
             ? sendEmail
-            :(e) => {
-              e.preventDefault();
-           
-            } 
+            : (e) => {
+                e.preventDefault();
+              }
         }
       >
         <div className="title">
@@ -230,9 +190,7 @@ import {ValidationInputTextareaTrue, ValidationInputTextareaFalse} from './../ac
         </div>
         <div className="contact-form">
           <div className="input-fields">
-            <label htmlFor="from_name">
-              Неисправан унос
-            </label>
+            <label htmlFor="from_name">Неисправан унос</label>
             <input
               onKeyUp={validationInputName}
               // onInvalid=this.setCustomValidity('Enter User Name Here')
@@ -261,7 +219,7 @@ import {ValidationInputTextareaTrue, ValidationInputTextareaFalse} from './../ac
               name="phone"
               required
             ></input>
-             <label htmlFor="subject">Неисправан унос</label>
+            <label htmlFor="subject">Неисправан унос</label>
             <input
               onKeyUp={validationInputSubject1}
               type="text"
@@ -308,25 +266,27 @@ import {ValidationInputTextareaTrue, ValidationInputTextareaFalse} from './../ac
           </p>
         </div> */}
       </form>
-        <Footer></Footer>
+      <Footer></Footer>
     </div>
   );
 }
 
-let mapStateToProps=(state)=>{
-  return {statusIzReduxa:state.status}
-}
+let mapStateToProps = (state) => {
+  return { statusIzReduxa: state.status };
+};
 
-
-let mapDispatchToProps = (dispatch) =>{
-
+let mapDispatchToProps = (dispatch) => {
   return {
-    setWholeStatusToFalse:()=>{dispatch(wholeStateToFalse())},
-    actionValidationInputTextareaTrue1:()=>{dispatch(ValidationInputTextareaTrue())},
-    ValidationInputTextareaFalse1:()=>{dispatch(ValidationInputTextareaFalse())}
+    setWholeStatusToFalse: () => {
+      dispatch(wholeStateToFalse());
+    },
+    actionValidationInputTextareaTrue1: () => {
+      dispatch(ValidationInputTextareaTrue());
+    },
+    ValidationInputTextareaFalse1: () => {
+      dispatch(ValidationInputTextareaFalse());
+    },
+  };
+};
 
-}
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Kontakt)
-
+export default connect(mapStateToProps, mapDispatchToProps)(Kontakt);
