ef32ec9cd31b992d5ba2b905cb6667e887c8ff9e
StevanZivadinovic <stevanzivadinovic@outlook.com>
2024-03-21 14:23:53 +0100

    add ejs view engine

--- node_simple_blog/app.js
+++ node_simple_blog/app.js
@@ -1,19 +1,22 @@
 //this is server in express, without ejs
 const express = require('express');
-
 const app=express();
 //register new engine
 app.set('view engine','ejs');
 app.listen(3000);
-
+//middlewares
+app.use(express.static('public'));
+// app.use((req, res, next)=>{
+//     console.log('every time this middleware renders');
+//     next()
+// })
 app.get('/',(req, res)=>{
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      ];
-    res.render('index', {title:'Home', blogs})
+   res.redirect('/blogs')
 })
+app.get('/blogs',(req, res)=>{
+    const blogs=[]
+    res.render('blogs',{title:"All blogs", blogs})
+ })
 app.get('/about',(req, res)=>{
 
     res.render('about', {title:'About'})
@@ -22,9 +25,12 @@ app.get('/blogs/create',(req, res)=>{
     res.render('create', {title:'Create a new blog'});
 })
 
+//middlewares
 app.use((req, res)=>{
     res.status(404).render('index')
 })
+
+
 
 
 
