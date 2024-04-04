6687da2106192f3a6312637f087258b5add2af27
StevanZivadinovic <stevanzivadinovic@outlook.com>
2024-04-04 17:02:00 +0200

    register added and navbar

--- mappin/backend/routes/users.ts
+++ mappin/backend/routes/users.ts
@@ -4,21 +4,31 @@ const User = require('./../models/User.ts');
 const bcrypt = require('bcrypt')
 
 usersRoutes.post('/register_new_user', async (req, res)=>{
+    try{
+ // Check if a user with the requested email already exists
+ const existingUser = await User.findOne({username:req.body.username, email: req.body.email });
+ if (existingUser) {
+   return res.status(400).json({ error: 'Email or username is already in use!' });
+ }
     // genrate new password
     const salt =await  bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(req.body.password, salt)
     //create new user
     const newUser = new User({
-        username:req.body.password,
+        username:req.body.username,
         email:req.body.email,
         password:hashedPassword,
     }) 
-    newUser.save()
-    .then((data)=>{
-    })
-    .catch((err)=>{
-        res.status(400).send(err);
-    })
+    await newUser.save();
+
+    console.log('New user registered:', newUser);
+    
+    // Send a response back to the frontend
+    return res.status(200).json({ message: 'User registered successfully' });
+  } catch (err) {
+    console.error('Error registering user:', err);
+    return res.status(400).json({ error: 'Error register, already user with that email or username' });
+  }
 
 })
 
