10d0dbe76f314577255cfbb4b2bddecdfa559dce
StevanZivadinovic <stevanzivadinovic@outlook.com>
2024-04-09 21:28:32 +0200

    add jwt token

--- mappin/backend/routes/users.js
+++ mappin/backend/routes/users.js
@@ -2,8 +2,17 @@
 const usersRoutes = require('express').Router();
 const { handleErrors } = require('../hlperFunctions/helperFunctions.js');
 const User = require('../models/User.ts');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+
+
+
+
+const maxAge = 3*24*60*60*1000;
+const createToken = (id)=>{
+  return jwt.sign({id}, 'sycret text of mine',{expiresIn:maxAge})
+}
 
 usersRoutes.post('/register_new_user', async (req, res)=>{
     try{
@@ -18,8 +27,9 @@ usersRoutes.post('/register_new_user', async (req, res)=>{
         email:req.body.email,
         password:req.body.password,
     }) 
-    await newUser.save();
-    
+   const newUserAdded =  await newUser.save();
+    const token = createToken(newUserAdded._id);
+     res.cookie('jwt',token, {httpOnly:true, maxAge:maxAge})
     // Send a response back to the frontend
     return res.status(200).json({ message: 'User registered successfully' });
   } catch (err) {
