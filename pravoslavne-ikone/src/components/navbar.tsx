2d9bdc5c883e695f48df051168f8d5db59b690ae
StevanZivadinovic <stevanzivadinovic@outlook.com>
2024-11-22 22:46:58 +0100

    navbar style from scss to tailwind

--- pravoslavne-ikone/src/components/navbar.tsx
+++ pravoslavne-ikone/src/components/navbar.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './../style/navbar.scss';
 import krst from './../assets/krst_beli.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
@@ -18,30 +17,32 @@ export default function Navbar() {
     <>
       <nav className="navbar bg-gradient-to-b from-customRed to-customGray h-[80px] flex justify-center items-center
       text-[1.2rem] w-full sticky top-0 z-[101]">
-        <div className="navbar-container flex justify-center items-center h-[80px]">
-          <div className="navbar-logo">
-            <Link to="/" className="navbar-logo-text" onClick={closeMobileMenu}>
+        <div className="mobile:w-full mobile:flex mobile:justify-between mobile:items-center flex justify-center items-center h-[80px]">
+          <div className="mobile:text-left mobile:w-[80%] mobile:text-[20px] mobile:justify-left flex items-left">
+            <Link to="/" className="mobile:text-[1.2rem] text-white justify-start self-center ml-[20px]
+            cursor-pointer text-[2rem] no-underline" onClick={closeMobileMenu}>
               {t('pravoslavne_ikone_nis')}
             </Link>
             <div>
-              <img alt="krst" id="krst" src={krst}></img>
+              <img alt="krst" className='mobile:hidden w-[50px] text-white' src={krst}></img>
             </div>
           </div>
 
-          <div className="menu-icon" onClick={handleClick}>
+          <div className="mobile:flex mobile:text-[1.8rem] mobile:mr-[1.5rem] cursor-pointer hidden" onClick={handleClick}>
             <FontAwesomeIcon icon={faBars} />
           </div>
-          <div className={!click ? 'sideBarWrapper' : 'sideBarWrapper active'}>
-            <div className='menu-language-side-bar'>
-           <LanguageNav cssClass={'language-select-ul-navbar'} width={40}/>
+          <div className={!click ? 'mobile:hidden' : ' mobile:z-[100] mobile:fixed mobile:left-0 mobile:top-0 mobile:h-[100vh] mobile:w-full mobile:flex-col mobile:justify-center mobile:items-center mobile:bg-[#242424fa]'}>
+            <div className='hidden mobile:absolute mobile:flex mobile:top-0 mobile:left-0 mobile:m-[1.5rem]
+            text-[2rem] w-[30%]'>
+           <LanguageNav cssClass={'mobile:flex mobile:list-none mobile:w-full'} width={40}/>
             </div>
-            <div className="menu-icon-side-navbar" onClick={closeMobileMenu}>
+            <div className="mobile:absolute mobile:flex mobile:top-0 mobile:right-0 mobile:m-[1.5rem] mobile:text-[2rem] mobile:cursor-pointer hidden" onClick={closeMobileMenu}>
               <FontAwesomeIcon icon={faTimes} />
             </div>
             <SideBarNavList closeMobileMenu={closeMobileMenu}/>
           </div>
         </div>
-        <LanguageNav cssClass={'language-select-ul'} width={24}/>
+        <LanguageNav cssClass={'mobile:hidden flex list-none'} width={24}/>
       </nav>
     </>
   );
--- pravoslavne-ikone/src/components/sideBarNavList.tsx
+++ pravoslavne-ikone/src/components/sideBarNavList.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom'
 import { sideBarNavListType } from '../types/types';
@@ -6,34 +5,42 @@ import { sideBarNavListType } from '../types/types';
 export const SideBarNavList = ({closeMobileMenu}:sideBarNavListType) => {
     const { t } = useTranslation();
   return (
-    <ul className="navmenuList">
-              <li className="nav-item">
-                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+    <ul className="mobile:justify-center mobile:h-full mobile:self-center mobile:flex mobile:flex-col mobile:items-center mobile:p-0 mobile:m-0 mobile:w-full flex flex-1 list-none text-center w-[60vw] justify-end mr-8">
+              <li className='mobile:w-full'>
+                <Link to="/" className="mobile:hover:rounded-none mobile:hover:text-[#242424] mobile:hover:bg-white mobile:text-center mobile:p-[2rem_0rem] mobile:w-full mobile:table text-white flex items-center 
+                no-underline p-[0rem_1rem] h-[100%] relative t-0 border-none border-0 hover:border-b-[2px] 
+                hover:border-solid hover:border-b-white" onClick={closeMobileMenu}>
                   {t('pocetna')}
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className='mobile:w-full'>
                 <Link
                   to="/ikonopisac"
-                  className="nav-links"
+                  className="mobile:hover:rounded-none mobile:hover:text-[#242424] mobile:hover:bg-white mobile:text-center mobile:p-[2rem_0rem] mobile:w-full mobile:table text-white flex items-center 
+                no-underline p-[0rem_1rem] h-[100%] relative t-0 border-none border-0 hover:border-b-[2px] 
+                hover:border-solid hover:border-b-white"
                   onClick={closeMobileMenu}
                 >
                   {t('o_ikonopiscu')}
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className='mobile:w-full'>
                 <Link
                   to="/galerija"
-                  className="nav-links"
+                  className="mobile:hover:rounded-none mobile:hover:text-[#242424] mobile:hover:bg-white mobile:text-center mobile:p-[2rem_0rem] mobile:w-full mobile:table text-white flex items-center 
+                  no-underline p-[0rem_1rem] h-[100%] relative t-0 border-none border-0 hover:border-b-[2px] 
+                  hover:border-solid hover:border-b-white"
                   onClick={closeMobileMenu}
                 >
                   {t('galerija')}
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className='h-[80px] mobile:w-full'>
                 <Link
                   to="/kontakt"
-                  className="nav-links"
+                  className="mobile:hover:rounded-none mobile:hover:text-[#242424] mobile:hover:bg-white mobile:text-center mobile:p-[2rem_0rem] mobile:w-full mobile:table text-white flex items-center 
+                no-underline p-[0rem_1rem] h-[100%] relative t-0 border-none border-0 hover:border-b-[2px] 
+                hover:border-solid hover:border-b-white"
                   onClick={closeMobileMenu}
                 >
                   {t('kontakt')}
